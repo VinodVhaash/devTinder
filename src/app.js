dadccd585d0d2501e973c9d43f35a611c239d794
VinodVhaash <vinodpatil2036@gmail.com>
2024-11-19 13:03:07 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ const express = require("express");
 const app = express();
 
 //request handler..showing res on browser...
+
+//this get will handle get api call
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vinod", lastName: "Patil" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data inserted successfully...!!!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Record deleted successfully...!!!");
+});
+
 app.use("/about", (req, res) => {
   res.send("Its an about page...");
 });
@@ -10,6 +24,10 @@ app.use("/about", (req, res) => {
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste node...");
+// });
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port number 3000...!!");
